b23e21c7fb3374fc4efe6b2249100f93d856198b
edteves <edwinjrteves29@gmail.com>
2024-03-18 21:42:30 +0800

    update code

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import css from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p className={css.footerText}>&copy; {new Date().getFullYear()}</p>
+    </footer>
+  );
+};
